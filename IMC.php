3ac57468f180ba973c43f4c1cb5657837eb685a0
Julián Rosalen <rosalenjulian@gmail.com>
2021-09-10 19:30:03 -0300

    IMC : remove php logic

--- IMC.php
+++ IMC.php
@@ -4,34 +4,21 @@
 
 <!DOCTYPE html>
 <html>
+
 <head>
-<title>Calculadora de índice de masa corporal</title>
+    <title>Calculadora de índice de masa corporal</title>
 </head>
+
 <body>
 
-<h1>Ingrese los datos solicitados para calcular su índice de masa corporal</h1>
+    <h1>Ingrese los datos solicitados para calcular su índice de masa corporal</h1>
 
-<form action = "IMC.php" method = "get">
-      
-    Altura en cm:  <input type="number" name= "altura">
-    Peso en kg: <input type= "number" name= "peso">
-    <input type="submit">    
-</form>
+    <form action="functions.php" method="post">
 
-<?php
-
-$altura = $_GET ["altura"];
-$weight = $_GET["peso"];
-
-$alturaDividida = ($altura / 100);
-
-$calculadoraMasaCorporal = $weight / ($alturaDividida * $alturaDividida);
-
-echo "Su índice de masa corporal es de $calculadoraMasaCorporal";
-
-?>
+        Altura en cm: <input type="number" name="altura" value=0>
+        Peso en kg: <input type="number" name="peso" value=0>
+        <input type="submit">
+    </form>
 </body>
+
 </html>
-
-
-
