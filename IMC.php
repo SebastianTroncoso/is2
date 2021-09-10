f4b3a62635f19f38f7ecd69d12ee8a5afcb7143d
SebastianTroncoso <sebastiantroncosovassallo@gmail.com>
2021-09-10 18:13:45 -0300

    agregar formulario en HTML

--- IMC.php
+++ IMC.php
@@ -2,4 +2,20 @@
 // Consigna del trabajo: crear una calculadora de IMC
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+<title>Calculadora de índice de masa corporal</title>
+</head>
+<body>
+
+<h1>Ingrese los datos solicitados para calcular su índice de masa corporal</h1>
+
+<form action = "IMC.php" method = "get">
+      
+    Altura en cm:  <input type="number" name= "altura">
+    Peso en kg: <input type= "number" name= "peso">
+    <input type="submit">    
+</form>
+
 
